22b18ed91f4d47358cc3406c1d1303c1412f67b6
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-11-13 18:38:26 -0300

    style: add FloatingActionButton, AlertDialog, ListView

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,6 +8,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List _taskList = ["Exemplo 1", "Exemplo 2", "Exemplo 3"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,19 +22,49 @@ class _HomeState extends State<Home> {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         backgroundColor: Colors.indigo,
-        onPressed: (){},
+        onPressed: (){
+           showDialog(
+              context: context,
+              builder: (context){
+                return AlertDialog(
+                  title: Text("Adicionar Tarefa"),
+                  content: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Digite a sua tarefa"
+                    ),
+                    onChanged: (text){
+
+                    },
+                  ),
+                  actions: [
+                    MaterialButton(
+                      child: Text("Cancelar"),
+                      onPressed:() => Navigator.pop(context),
+                    ),
+                    MaterialButton(
+                        child: Text("Salvar"),
+                      onPressed:(){
+                        Navigator.pop(context);
+                      }
+                    ),
+                  ],
+                );
+              }
+          );
+        },
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Center(
-            child: Text(
-              "Body",
-              style: TextStyle(
-                  fontSize: 33,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
+          Expanded(
+              child: ListView.builder(
+                itemCount: _taskList.length,
+                  itemBuilder: (context, index){
+                    return ListTile(
+                      title: Text(_taskList[index]),
+                    );
+                  }
+              )
           ),
         ],
       ),
